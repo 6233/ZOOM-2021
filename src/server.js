5584dd764058de81c0de7c318fdce08a0a7f2e25
jhleeO <47955896+jhleeO@users.noreply.github.com>
2021-12-28 14:59:48 +0900

    WebSocket

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from  "path";
+import WebSocket, {WebSocketServer} from "ws";
+import http from "http";
 
 const app = express();
 const __dirname = path.resolve("./src");
@@ -7,8 +9,14 @@ const __dirname = path.resolve("./src");
 app.set("view engine", "pug")
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+wss.on("connection", (socket) => {console.log(socket)})
+
+server.listen(3000, handleListen);
